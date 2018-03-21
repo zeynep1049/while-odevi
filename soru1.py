a5c8d2bd63a6a2474da21b62dd0e531544df676c
zeynep1049 <37124318+zeynep1049@users.noreply.github.com>
2018-03-21 23:07:34 +0300

    python fonksiyonlar 2 ödev, 1.soru

---
+++ soru1.py
@@ -0,0 +1,60 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Finansman Geliri Giriniz1000
+Pazar Geliri Giriniz50000
+Kira Geliri Giriniz800000
+Ücret Giriniz40000
+Finansman Gideri Giriniz90000
+Pazar Gideri Giriniz10000
+Muhasebe Gideri Giriniz222222
+Şirket kar yapmıştır.
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Finansman Geliri Girinizlsllsls
+Traceback (most recent call last):
+  File "C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py", line 16, in <module>
+    x=int(input("Finansman Geliri Giriniz"))
+ValueError: invalid literal for int() with base 10: 'lsllsls'
+>>> 
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Finansman Geliri Giriniz1
+Pazar Geliri Giriniz2
+Kira Geliri Giriniz3
+Ücret Giriniz4
+Finansman Gideri Giriniz5
+Pazar Gideri Giriniz6
+Muhasebe Gideri Giriniz7
+Traceback (most recent call last):
+  File "C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py", line 23, in <module>
+    if (x+y+z)-(a+b+c+d)>5500:
+NameError: name 'a' is not defined
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Finansman Geliri Giriniz1
+Pazar Geliri Giriniz2
+Kira Geliri Giriniz3
+Ücret Giriniz4
+Finansman Gideri Giriniz5
+Pazar Gideri Giriniz6
+Muhasebe Gideri Giriniz7
+Şirket zarar etmiştir.
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Toplam satış giriniz:20
+Hammadde maliyetini giriniz:50
+Bakım onarım giriniz:40000
+Sevkiyat giriniz:555555
+Satın alınan hizmet giriniz:555556
+İşletme katma değer cirosu düşüktür. Katma değer cironuz: -1151141
+>>> 
+ RESTART: C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py 
+Toplam satış giriniz:2
+Hammadde maliyetini giriniz:213254,
+Traceback (most recent call last):
+  File "C:/Users/casper/AppData/Local/Programs/Python/Python36-32/python soru 1.py", line 6, in <module>
+    b = int(input("Hammadde maliyetini giriniz:"))
+ValueError: invalid literal for int() with base 10: '213254,'
+>>> 
